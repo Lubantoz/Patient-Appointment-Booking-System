1289d69364f030fe48c949eb4d820096f800693b
luntozicpt024 <luntozicpt024@student.wethinkcode.co.za>
2025-11-15 21:51:33 +0200

    added 3 classes

--- src/main/java/com/appointments/models/Doctor.java
+++ src/main/java/com/appointments/models/Doctor.java
@@ -10,7 +10,7 @@ public class Doctor {
         this.name = name;
         this.specialist = specialist;
     }
-//git
+//gitt
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
